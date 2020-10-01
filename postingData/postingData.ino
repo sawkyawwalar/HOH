7f0b3ff60d6a28ab8135f67064923a7a4f1ecfa4
SawKyaw <sawkyawwalarhtwe123@gmail.com>
2020-10-01 19:43:14 +0630

    Add files via upload

---
+++ postingData/postingData.ino
@@ -0,0 +1,33 @@
+#include <WiFiClient.h> 
+#include <ESP8266WiFi.h>
+const char *ssid     = "your-ssid";
+const char *password = "your-password";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop() {
+}
